fdf01e322c3449de863a87c3f3e4bd6117979f76
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 00:17:55 -0500

    added error handling

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const res = await fetch("https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec")
-.then(e => e.json)
+.then(
+    e=> {
+        if(e.ok) {
+            console.log("👍")
+            e.json();
+        }
+        else {
+            console.error("👎")
+        }
+    }
+)
 .then(e => console.log(e))
 
 // for(let i = 0; i <= data.body.Title.length; i++) {
