3045b09f58b177a09942d292590af53d11ca7a76
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 13:00:15 -0500

    added favicon and styling

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
 
 function makeCards(e) {
-    for(let i = 0; i <= e.length; i++) {
+    for(let i = 0; i < e.length; i++) {
+        console.log(i)
         document.getElementById("root").innerHTML += `<div class="card"><h1>${e[i].Title}</h1><p>${e[i].Content}</p></div>`
     }
 }
