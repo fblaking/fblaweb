e129a5cf37f43e62725ef68a6cb56d96f2e87ebe
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 11:13:35 -0500

    fixed styling issues

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
+
+function makeCards(e) {
+    for(let i = 0; i <= e.length; i++) {
+        document.getElementById("root").innerHTML += `<div class="card"><h1>${e[i].Title}</h1><p>${e[i].Content}</p></div>`
+    }
+}
+
 async function fetchData(e) {
     await fetch(e)
     .then(res => res.json())
     .then(data => {
-        for(let i = 0; i <= data.length; i++) {
-        console.log("👍")
-        document.getElementById("root").innerHTML += `<div className="card"><h1>${data[i].Title}</h1><p>${data[i].Content}</p><img src=${data[i].Image} /></div>`
-    }
+        
     })
 }
 fetchData(googleAppScriptUrl)
