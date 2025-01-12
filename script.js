8a5711a7142649c0b1b65f5b59ffccac83560697
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 02:08:06 -0500

    updated for loop

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function fetchData(e) {
     await fetch(e)
     .then(res => res.json())
     .then(data => {
-        for(let i = 0; i <= data.length; i++) {
+        for(let i = -1; i <= data.length; i++) {
         console.log("👍")
         document.getElementById("root").innerHTML = `<div className="card"><h1>${data[i].Title}</h1><p>${data[i].Content}</p><img src=${data[i].Image} /></div>`
     }
