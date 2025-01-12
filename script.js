43118b751e6807d3d1ebcc4dbaa3d12f9e3756b5
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 00:29:27 -0500

    did something else

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const res = fetch("https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec")
-.then(
-    e=> {
-        if(e.ok) {
-            console.log("👍")
-            e.json();
-        }
-        else {
-            console.error("👎")
-        }
-    }
-)
-.then(e => console.log(e))
+fetch("https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec")
+.then(response=>{
+   response.json()
+}) 
+.then(json=>console.log(json))
+.catch(error=>console.log('something went wrong'))
+
+// .then(
+//     e=> {
+//             if(e.ok) {
+//             console.log("👍")
+//             e.json();
+//             }
+//             else {
+//             console.error("👎")
+//             }
+//         }
+// )
+// .then(e => console.log(e))
 
 // for(let i = 0; i <= data.body.Title.length; i++) {
 //     document.getElementById("root").innerHTML = ` <div className="card"><h1>${data.body.Tit}</h1><p>{content}</p></div>`
