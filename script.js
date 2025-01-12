9b5cd74e71c251f1fd346c59548620a1d6a28cf7
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 01:30:50 -0500

    changed for loop positioning

--- script.js
+++ script.js
@@ -9,13 +9,12 @@ async function fetchData(e) {
         datalen = data.length
         output = data
     })
-        
+   for(let i = 0; i <= datalen; i++) {
+    document.getElementById("root").innerHTML = `<div className="card"><h1>${output.data[i].Title}</h1><p>${output.data[i].Content}</p><img src=${output.data[i].Image} /></div>`
+    console.log(`👍 ${i}`)
+    }     
 }
 
 fetchData(googleAppScriptUrl)
 
 
-for(let i = 0; i <= datalen; i++) {
-    document.getElementById("root").innerHTML = `<div className="card"><h1>${output.data[i].Title}</h1><p>${output.data[i].Content}</p><img src=${output.data[i].Image} /></div>`
-    console.log(`👍 ${i}`)
-}
