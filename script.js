505d4641ca341e03cfa063ecf66ce3908377b086
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 02:03:14 -0500

    5678

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
-
-let datalen;
-let output;
 async function fetchData(e) {
     await fetch(e)
     .then(res => res.json())
     .then(data => {
-        datalen = data.length
-        output = data
-    })
-    for(let i = 0; i < datalen; i++) {
-        console.log("insidefor loop")
-        document.getElementById("root").innerHTML = `<div className="card"><h1>${output.data[i].Title}</h1><p>${output.data[i].Content}</p><img src=${output.data[i].Image} /></div>`
-        
+        for(let i = 0; i < data.length; i++) {
+        console.log("👍")
+        document.getElementById("root").innerHTML = `<div className="card"><h1>${data[i].Title}</h1><p>${data[i].Content}</p><img src=${data[i].Image} /></div>`
     }
+    })
 }
 fetchData(googleAppScriptUrl)
-// function makeCard() {
-//     console.log("datalen " + output.length)
-         
-// }
-
-
-// makeCard()
