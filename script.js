28273d6ae0dc90ac89af7cfdacac348eb2722861
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 00:49:53 -0500

    figured it out

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const googleAppScriptUrl = "https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec"
 
-fetch(googleAppScriptUrl)
-.then(response => response.json())
-.then(data => setRecords(data))
-.then(() => setIsDataLoading(false))                
-.catch(err => console.log(err));
+async function fetchData(e) {
+    fetch(e)
+    .then(
+        a=> {
+                if(a.ok) {
+                console.log("👍")
+                a.json();
+                }
+                else {
+                console.error("👎")
+                }
+            }
+    )
+    .then(a => console.log(a))
+}
 
-// .then(
-//     e=> {
-//             if(e.ok) {
-//             console.log("👍")
-//             e.json();
-//             }
-//             else {
-//             console.error("👎")
-//             }
-//         }
-// )
-// .then(e => console.log(e))
+
+
 
 // for(let i = 0; i <= data.body.Title.length; i++) {
 //     document.getElementById("root").innerHTML = ` <div className="card"><h1>${data.body.Tit}</h1><p>{content}</p></div>`
