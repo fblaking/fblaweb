53c404810154fffc7f3c6ba4f1d60141f825e42d
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 01:23:54 -0500

    redid output

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
 
+let datalen;
+let output;
 async function fetchData(e) {
     await fetch(e)
     .then(res => res.json())
-    .then(data => console.log(data.length))
+    .then(data => {
+        datalen = data.length
+        output = data
+    })
         
 }
 
-const output = fetchData(googleAppScriptUrl)
+fetchData(googleAppScriptUrl)
 
 
-for(let i = 0; i <= output.data.length; i++) {
+for(let i = 0; i <= datalen; i++) {
     document.getElementById("root").innerHTML = `<div className="card"><h1>${output.data[i].Title}</h1><p>${output.data[i].Content}</p><img src=${output.data[i].Image} /></div>`
 }
