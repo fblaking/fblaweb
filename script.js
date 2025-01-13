afb22a063f27e3d148dcd7ca349ebc78cb1f0c94
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-13 09:39:31 -0500

    made date be able to change

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
+const d = new Date();
+let year = d.getFullYear();
 // put in for loop: <img src=${e[i].Image} />     👍
 function makeCards(e) {
     for(let i = 0; i < e.length; i++) {
         console.log(i)
         document.getElementById("root").innerHTML += `<div class="card"><h1>${e[i].Title}</h1><p>${e[i].Content}</p></div>`
     }
+}
+
+function dateFooter() { 
+    document.getElementById("footer").innerHTML = `<p>&copy; ${year} FBLA Project. All Rights Reserved.</p><br /><p>Made by Krishna Murthy, Karthikeya Soora, and Pranav Sasikumar.</p>`
 }
 
 async function fetchData(e) {
