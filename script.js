70e9dc821a40929de7a74c2d1901043c9cb63339
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-12 22:23:07 -0500

    added code for later use

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
-
+// put in for loop: <img src=${e[i].Image} />     👍
 function makeCards(e) {
     for(let i = 0; i < e.length; i++) {
         console.log(i)
