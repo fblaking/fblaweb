b934a5d272355ca20386cfd80346a562c2b84e22
Krishna G. Murthy <76011227+iwillbemicrosoftceo@users.noreply.github.com>
2025-01-13 10:14:12 -0500

    added image and fixed footer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const googleAppScriptUrl = 'https://script.google.com/macros/s/AKfycbzKczUtTVXfohypV-hL1EDAFJEm2CpyRfAHJhi7CsOEQ2djjK7bRuXLqX-6wWjicD2h/exec'
 const d = new Date();
 let year = d.getFullYear();
-// put in for loop: <img src=${e[i].Image} />     👍
+
 function makeCards(e) {
     for(let i = 0; i < e.length; i++) {
         console.log(i)
-        document.getElementById("root").innerHTML += `<div class="card"><h1>${e[i].Title}</h1><p>${e[i].Content}</p></div>`
+        document.getElementById("root").innerHTML += `<div class="card"><h1>${e[i].Title}</h1><p>${e[i].Content}</p><img src=${e[i].Image} /></div>`
     }
 }
 
@@ -19,3 +19,4 @@ async function fetchData(e) {
     .then(d => makeCards(d))
 }
 fetchData(googleAppScriptUrl)
+dateFooter()
